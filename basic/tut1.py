44b8a00d79b1d8e4a001b0c1e012fe7385580d9b
singzinc <sing.zinc.hk@gmail.com>
2017-11-05 22:37:17 +0800

    update the structure

--- basic/tut1.py
+++ basic/tut1.py
@@ -116,3 +116,14 @@ print(ob.func)
 # Calling function func()
 # Output: Hello
 ob.func()
+
+
+
+# ===============================
+
+for x in range(1, 11):
+        print(repr(x).rjust(2), repr(x*x).rjust(3), end=' ')
+        # Note use of 'end' on previous line
+        print(repr(x*x*x).rjust(4))
+        print('xxxxx')
+print('test')
