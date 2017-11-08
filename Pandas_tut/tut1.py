6acf00628f7760a6f3e814e75e0950d4cc7e2d60
singzinc <sing.zinc.hk@gmail.com>
2017-11-09 00:23:48 +0800

    update the example

--- Pandas_tut/tut1.py
+++ Pandas_tut/tut1.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
+##################################################
+#The Series Data Structure
+
 #################### example 1 ####################
 numbers = {1,2,3,4,5}
 ser = pd.Series(list(numbers))
-
-print(ser.size)
+print(ser)
+print("size of the list" , ser.size)
 
 ##################  example 2 ###################
 animals = ['Tiger', 'Bear', 'Moose']
 print(pd.Series(animals))
 
+
+##################  example 3 ###################
+numbers = [1, 2, None]
+pd.Series(numbers)
+print(pd.Series(numbers))
